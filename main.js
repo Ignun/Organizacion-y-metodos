02a9c58eb73ad1950af05871a1ee270df544207e
unknown <lucas.cruz2003@hotmail.com>
2022-10-17 19:45:27 -0300

    a

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function variables() {
   // Variables
-  var nombre = document.getElementById("nombre").value;
+  let nombre = document.getElementById("nombre").value;
   let apellido = document.getElementById("apellido").value;
   let cuil = document.getElementById("cuil").value;
   let legajo = document.getElementById("legajo").value;
@@ -13,10 +13,22 @@ function variables() {
   let sueldobasico = document.getElementById("sueldobasico").value;
 }
 
-function generar(nombre) {
+function generar() {
+  variables();
   let conf = confirm("Esta seguro de los datos ingresados?");
   if (conf == true) {
-    let newtext = document.getElementById("blankid");
-    newtext.innerHTML = "<p>Nombre:" + nombre + "</p>";
+    let resultText = document.getElementById("blankid");
+    resultText.innerHTML =
+      "<p>Nombre: " +
+      nombre.value +
+      "</p> <p>Apellido:" +
+      apellido.value +
+      "</p> <p class=bold">CUIL:" +
+      cuil.value +
+      "</p> <p>Legajo:" +
+      legajo.value +
+      "</p> <p>Categoria:" +
+      categoria.value +
+      "</p>";
   }
 }
